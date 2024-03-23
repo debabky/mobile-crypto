4eced4727e34ef1b79d55540c14d1275bcc880ca
yivanov <apereliez1@gmail.com>
2024-03-23 23:39:08 +0200

    fix for mobile

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module mobile-crypto
+module Denchik
 
 go 1.21.4
 
@@ -7,5 +7,8 @@ require github.com/iden3/go-iden3-crypto v0.0.16
 require (
 	github.com/dchest/blake512 v1.0.0 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
-	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/mobile v0.0.0-20240320162201-c76e57eead38 // indirect
+	golang.org/x/mod v0.16.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/tools v0.19.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+package Denchik
 
 import (
 	"math/big"
@@ -7,44 +7,48 @@ import (
 	"github.com/iden3/go-iden3-crypto/poseidon"
 )
 
-type Crypto struct{}
+type DenchikC struct{}
 
-func (c *Crypto) EdDSAKeyPairGen() (babyjub.PrivateKey, babyjub.PublicKey) {
+func (c DenchikC) New() *DenchikC {
+	return &DenchikC{}
+}
+
+func (c *DenchikC) EdDSAKeyPairGen() (babyjub.PrivateKey, babyjub.PublicKey) {
 	privateKey := babyjub.NewRandPrivKey()
 	pubKey := privateKey.Public()
 
 	return privateKey, *pubKey
 }
 
-func (c *Crypto) PoseidonHashBytes(data []byte) *big.Int {
+func (c *DenchikC) PoseidonHashBytes(data []byte) []byte {
 	inputBigInt := big.NewInt(0)
 	inputBigInt.SetBytes(data)
 	inputArr := []*big.Int{inputBigInt}
 
 	hash, _ := poseidon.Hash(inputArr)
 
-	return hash
+	return hash.Bytes()
 }
 
-func (c *Crypto) PoseidonHash(input *big.Int) *big.Int {
+func (c *DenchikC) PoseidonHash(input *big.Int) []byte {
 	inputArr := []*big.Int{input}
 	hash, _ := poseidon.Hash(inputArr)
 
-	return hash
+	return hash.Bytes()
 }
 
-func (c *Crypto) PoseidonHashLeftRight(left *big.Int, right *big.Int) *big.Int {
+func (c *DenchikC) PoseidonHashLeftRight(left *big.Int, right *big.Int) []byte {
 	input := []*big.Int{left, right}
 	hash, _ := poseidon.Hash(input)
 
-	return hash
+	return hash.Bytes()
 }
 
-func (c *Crypto) PoseidonHashPoint(point *babyjub.Point) *big.Int {
+func (c *DenchikC) PoseidonHashPoint(point *babyjub.Point) []byte {
 	return c.PoseidonHashLeftRight(point.X, point.Y)
 }
 
-func (c *Crypto) EdDSASignature(privKey babyjub.PrivateKey, signData *big.Int) *babyjub.Signature {
+func (c *DenchikC) EdDSASignature(privKey babyjub.PrivateKey, signData *big.Int) *babyjub.Signature {
 	signature := privKey.SignPoseidon(signData)
 	return signature
 }
